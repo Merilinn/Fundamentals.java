4eb5f645a063f6778a8fb6628404fb68d0fd9ecb
Merilinn <merian.konyarska@gmail.com>
2023-03-02 14:03:51 +0200

    up

--- Problems/src/AssociativeArrays/P03OddOccurrences_Lab.java
+++ Problems/src/AssociativeArrays/P03OddOccurrences_Lab.java
@@ -1,9 +1,31 @@
 package AssociativeArrays;
 
-import java.util.Scanner;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.*;
 
 public class P03OddOccurrences_Lab {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String[] wordArr = scan.nextLine().toLowerCase().split(" ");
+
+        Map <String, Integer> wordMap = new LinkedHashMap<>();
+        for (String word : wordArr) {
+            Integer countMap = wordMap.get(word);
+            if (!wordMap.containsKey(word)){
+                wordMap.put(word, 1);
+            }   else {
+                wordMap.put(word, countMap + 1);
+            }
+        }
+        List <String> oddWords = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+            if (entry.getValue() % 2 != 0){
+                oddWords.add(entry.getKey());
+            }
+        }
+        System.out.println(String.join(", ", oddWords));
+
+
     }
 }
---
+++ Problems/src/AssociativeArrays/P04WordFilter_Lab.java
@@ -0,0 +1,13 @@
+package AssociativeArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04WordFilter_Lab {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String [] wordsArr = Arrays.stream(scan.nextLine().split(" ")).filter(e -> e.length() % 2 == 0).toArray(String[]::new);
+
+        System.out.println(String.join(System.lineSeparator(), wordsArr));
+    }
+}
